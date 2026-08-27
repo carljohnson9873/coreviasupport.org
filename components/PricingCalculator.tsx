486ec48deb0ca156eca9221e94c00139b0710e9c
Carl Johnson <carljhonson4758@gmail.com>
2026-08-27 11:59:45 +0530

    Update pricing calculator rates

--- components/PricingCalculator.tsx
+++ components/PricingCalculator.tsx
@@ -5,8 +5,8 @@ import { useMemo, useState } from "react";
 
 const MONTHLY_PRICE_PER_SERVICE = 1199;
 const SETUP_PRICE_PER_SERVICE = 500;
-const MULTI_SERVICE_SETUP_PRICE = 1000;
-const FIRST_CLIENT_DISCOUNT = 30;
+const MULTI_SERVICE_SETUP_PRICE = 750;
+const FIRST_CLIENT_DISCOUNT = 40;
 
 const services = [
   "Order Management",
